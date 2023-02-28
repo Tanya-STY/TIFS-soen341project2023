faa95fab70936cdd3934f602a76baaa35a9b7ba0
Tanya-STY <tanya.sty99@gmail.com>
2023-02-27 23:59:43 -0500

    NavBar and Search Page

--- ProFolia/src/App.js
+++ ProFolia/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import HomePage from './components/HomePage';
 import MainPage from './components/MainPage/MainPage';
+import Header from './components/Header/Header';
+import JobLists from "./components/Jobs/JobLists";
+import JobDetails from "./components/Job-details/JobDetails";
+
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-      <MainPage/>
-      {/* <HomePage/> */}
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Navigate to='/jobs' />} />
+        <Route path='/jobs' element={<JobLists />} />
+        <Route path='/jobs/:position' element={<JobDetails />} />
+      </Routes>
     </div>
   );
 }
 export default App;
+
