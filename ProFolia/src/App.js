8df3cd62bd118eeb3d49a068d6e81ab012aed61e
Tanya-STY <tanya.sty99@gmail.com>
2023-03-01 20:56:53 -0500

    Updated the JobPage to its own folder

--- ProFolia/src/App.js
+++ ProFolia/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import HomePage from './components/HomePage';
 import MainPage from './components/MainPage/MainPage';
-import Header from './components/Header/Header';
-import JobLists from "./components/Jobs/JobLists";
-import JobDetails from "./components/Job-details/JobDetails";
+// import Header from './components/JobsPage/Header/Header';
+// import JobLists from "./components/Jobs/JobLists";
+// import JobDetails from "./components/Job-details/JobDetails";
+import JobsPage from './components/JobsPage/JobsPage';
 
 import { Routes, Route, Navigate } from 'react-router-dom'
 
@@ -11,12 +12,12 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 function App() {
   return (
     <div>
-      <Header />
-      <Routes>
+      <JobsPage />
+      {/* <Routes>
         <Route path='/' element={<Navigate to='/jobs' />} />
         <Route path='/jobs' element={<JobLists />} />
         <Route path='/jobs/:position' element={<JobDetails />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
