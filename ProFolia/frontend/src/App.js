b0cd47639a81453ffb191d7e9df7e4d1e64c5b57
Tanya So Tin Yan <95064474+Tanya-STY@users.noreply.github.com>
2023-03-25 22:06:14 -0400

    Updated the paths
    Updated the paths so that Jobs in the Nav is connected to the corresponding page.

--- ProFolia/frontend/src/App.js
+++ ProFolia/frontend/src/App.js
@@ -3,16 +3,7 @@ import logo from './logo.svg';
 // import React, { useEffect } from 'react';
 // import { observer } from 'mobx-react';
 // import HomePage from './components/HomePage';
-// import MainPage from './components/MainPage/MainPage';
-// import Login from './components/pages/Login';
-// import Register from './components/pages/Register';
-// // import Header from './components/JobsPage/Header/Header';
-// import JobLists from "./components/JobsPage/Jobs/JobLists";
-// import JobDetails from "./components/JobsPage/Job-details/JobDetails";
-// // import JobsPage from './components/JobsPage/JobsPage';
-// import UserStore from "./stores/UserStore";
-//
-// import AddJobForm from './components/AddJobForm/AddJobForm';
+
 import './App.css';
 
 import React from 'react';
@@ -25,16 +16,25 @@ import MainPage from "./components/MainPage/MainPage";
 import TypeUser from "./components/TypeUser";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
+import EmployerProfile from "./components/EmployerProfile/EmployerProfile"
+import JobsPage from "./components/JobsPage/JobsPage"
+import JobLists from './components/JobsPage/Jobs/JobLists';
+import JobDetails from './components/JobsPage/Job-details/JobDetails';
 
 function App() {
   return (
       // <BrowserRouter>
         <Routes>
+        
             <Route path='/' element={<MainPage/>} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/homepage' element={<Homepage />} />
-            <Route path='/typeuser' element={<TypeUser />} />
+
+            <Route path="/jobs/*" element={<JobsPage />} />
+
+
+            {/* <Route path='/typeuser' element={<TypeUser />} /> */}
             <Route path='/dashboard' element={<Dashboard />} />
           {/*<Route path="/dashboard">*/}
           {/*  <Navbar/>*/}
@@ -46,19 +46,3 @@ function App() {
 }
 
 export default App;
-
-// return (
-//     <div>
-//       {/*<Login/>*/}
-//       {/*<AddJobForm/>*/}
-//       <Routes>
-//         <Route path='/' element={<MainPage />} />
-//         <Route path='/home' element={<HomePage />} />
-//         <Route path='/login' element={<Login />} />
-//         <Route path='/register' element={<Register />} />
-//         <Route path='/submit-form' element={<AddJobForm />} />
-//         <Route path='/jobs' element={<JobLists />} />
-//         <Route path='/jobs/:position' element={<JobDetails />} />
-//       </Routes>
-//     </div>
-//   );
